5023b10cf15b7bbd66d5ea536126caedd9d6fe26
Pedro Winter <pedro.winter.sc@gmail.com>
2025-08-10 19:46:41 -0300

    Logica pronta

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
+import pandas as pd
 
-codSaco = "205.0015"
-peso_fardo_Saco = 7.136
-qntd_de_saco_por_fardo = 500
-peso_do_saco_para_guardar_embalgem = 0.02
+banco = pd.read_csv("dados.csv", sep=";")  
+contador=0
+dadosValidos = banco.dropna(how='all')
+qntdLinha = len(dadosValidos)
+codSaco = banco.loc[contador, "codSaco"]
+
+pesoEmbalgemSaco = 0.02
+verificaRes = False
+
+while verificaRes == False:
+    perguntacodSaco = input("Qual é o codigo do saco? ")
+
+    while  contador < qntdLinha:
+        codSaco = banco.loc[contador, "codSaco"]
+    
+        if perguntacodSaco == codSaco:
+
+            print(codSaco)
+            pesoFardo = banco.loc[contador, "pesoFardo"]
+            print(pesoFardo)
+            qntdFardo = banco.loc[contador,"qntdFardo"]
+            qntdSeparacao = int(input("Qual a quantidade que voce precisa separar? "))
+            peso_da_qtnd_do_saco = ((qntdSeparacao*pesoFardo/qntdFardo)+pesoEmbalgemSaco)*1.03
+            print(f"Peso de separacao e {peso_da_qtnd_do_saco:.2f} do saco {codSaco}")
+            contador = qntdLinha
+            verificaRes = True
+            
+        else:
+            contador += 1
+
+    if verificaRes == False:
+        print("Esse codigo de SACO NAO EXISTE, tente outro CODIGO")
+        contador= 0   
 
 
 
-qntd_de_saco_para_separar = int(input("Qual a quantidade que voce precisa separar? "))
 
 
-peso_da_qtnd_do_saco = ((qntd_de_saco_para_separar*peso_fardo_Saco/qntd_de_saco_por_fardo)+peso_do_saco_para_guardar_embalgem)*1.03
 
-print(f"O peso de {qntd_de_saco_para_separar} é {peso_da_qtnd_do_saco:.2f}")
+
 
 
 
